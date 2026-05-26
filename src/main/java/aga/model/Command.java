dc7a98b18df364048a1152d031dff544ae35896e
Aga <agkop01@gmail.com>
2026-05-26 15:17:17 +0200

    added getters and setters

--- src/main/java/aga/model/Command.java
+++ src/main/java/aga/model/Command.java
@@ -1,6 +1,7 @@
 package aga.model;
 
 import java.io.Serializable;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,10 +11,69 @@ import jakarta.persistence.Id;
 public class Command implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
     private Long id;
     private String name;
+    private String description;
     private String language;
     private String framework;
     private String[] tag;
     private String version;
+   
+    public Command(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getFramework() {
+        return framework;
+    }
+    public void setFramework(String framework) {
+        this.framework = framework;
+    }
+
+    public String[] getTag() {
+        return tag;
+    }
+
+    public void setTag(String[] tag) {
+        this.tag = tag;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+    
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+
 }
