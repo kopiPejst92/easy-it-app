13a0803e814813a8f05f98847d815af9406b6462
Aga <agkop01@gmail.com>
2026-05-26 09:44:34 +0200

    first commit

---
+++ src/main/java/aga/easyit/EasyItApplication.java
@@ -0,0 +1,13 @@
+package aga.easyit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aga/easyit/easy_it_app/EasyItAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aga.easyit.easy_it_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyItAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
