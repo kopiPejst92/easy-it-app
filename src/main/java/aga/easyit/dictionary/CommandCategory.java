8b4ae2b3c15b2be2f0abba15de4d66721a9f91c5
Aga <agkop01@gmail.com>
2026-06-12 13:07:25 +0200

    added category plus some edition in command

---
+++ src/main/java/aga/easyit/dictionary/CommandCategory.java
@@ -0,0 +1,13 @@
+package aga.easyit.dictionary;
+
+public enum CommandCategory{ 
+    FILE_MANAGEMENT("File management"),
+    SYSTEM_COMMANDS("System commands");
+
+    private String command
+
+    Category(String string) {
+        
+    }
+
+}
