b4ae28cb90c9806ab776414f8ef6a220615a1a86
Aga <agkop01@gmail.com>
2026-07-15 11:17:04 +0200

    added examples of commands and updated command mapper

--- backend/src/main/java/aga/easyit/mapper/CommandMapper.java
+++ backend/src/main/java/aga/easyit/mapper/CommandMapper.java
@@ -1,11 +1,22 @@
 package aga.easyit.mapper;
 
 import org.mapstruct.Mapper;
-
 import aga.easyit.dto.CommandDTO;
 import aga.easyit.model.Command;
 
 @Mapper(componentModel="spring")
-public interface CommandMapper extends GenericMapper<Command, CommandDTO>{
+public abstract class CommandMapper{
+    
+    public abstract CommandDTO toDto(Command command);
 
+    public abstract Command toEntity(CommandDTO commandDTO);
+
+
+    public Command toEntityFromRawString(String rawSyntaxString){
+        String[] tokens=rawSyntaxString.trim().split("//s+");
+        String firstToken = tokens[0];
+        //TODO parsowanie bazy w zaleznosci czy to funkcja w bibliotece czy command w terminalu
+        Command parsedCommand = new Command(firstToken, "Parsed command");
+        return parsedCommand;
+    }
 }
