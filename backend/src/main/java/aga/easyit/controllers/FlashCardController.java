6b682d5cdf8ed7569a37f04b21a76bd72346eb85
Aga <agkop01@gmail.com>
2026-07-16 10:45:04 +0200

    Add RestController for FlashCards

---
+++ backend/src/main/java/aga/easyit/controllers/FlashCardController.java
@@ -0,0 +1,35 @@
+package aga.easyit.controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import aga.easyit.model.FlashCard;
+import aga.easyit.service.FlashCardService;
+
+@RestController
+@RequestMapping("/flashcard")
+public class FlashCardController {
+    private final FlashCardService flashCardService;
+
+    public FlashCardController(FlashCardService flashCardService) {
+        this.flashCardService = flashCardService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<FlashCard>> getAllFlashCards(){
+        List<FlashCard> flashCards= flashCardService.findAllFlashCards();
+        return new ResponseEntity<>(flashCards, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<List<FlashCard>> getFlashCardById(@PathVariable("id") Long id){
+        List<FlashCard> flashCards= flashCardService.findAllFlashCards();
+        return new ResponseEntity<>(flashCards, HttpStatus.OK);
+    }
+}
