d875f121d6da65ec2f7154eeb3bf1ef42455acca
Aga <agkop01@gmail.com>
2026-07-16 10:06:43 +0200

    Returned to taking care of restAPI

--- backend/src/main/java/aga/easyit/service/CommandService.java
+++ backend/src/main/java/aga/easyit/service/CommandService.java
@@ -1,14 +1,9 @@
 package aga.easyit.service;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
-import aga.easyit.dto.ArgumentDTO;
 import aga.easyit.dto.CommandDTO;
-import aga.easyit.mapper.ArgumentMapper;
 import aga.easyit.mapper.CommandMapper;
-import aga.easyit.model.Argument;
 import aga.easyit.model.Command;
 import aga.easyit.repo.CommandRepository;
 
@@ -16,46 +11,15 @@ import aga.easyit.repo.CommandRepository;
 public class CommandService{
     private final CommandRepository commandRepository;
     private final CommandMapper commandMapper;
-    private final ArgumentMapper argumentMapper;
 
-    public CommandService(CommandRepository commandRepository, CommandMapper commandMapper, ArgumentMapper argumentMapper){
+    public CommandService(CommandRepository commandRepository, CommandMapper commandMapper){
         this.commandRepository=commandRepository;
         this.commandMapper=commandMapper;
-        this.argumentMapper=argumentMapper;
-    } 
-
-    public Command createCommandFromRawSyntax(String raw){
-        Command parsedCommand = commandMapper.toEntityFromRawString(raw);
-        return commandRepository.findCommandBySyntax(parsedCommand.getSyntax()).map()
     }
 
-    public Command getOrCreateCommand(CommandDTO commandDTO, List<ArgumentDTO> lArgumentDTOs){
-       return commandRepository.findCommandBySyntax(commandDTO.syntax()).map(existingCommand -> { 
-        if(lArgumentDTOs!=null && !lArgumentDTOs.isEmpty()){
-            updateCommandArguments(existingCommand, lArgumentDTOs);
-        }
-        return existingCommand;
-       }).orElseGet(() ->{ 
+    public Command getOrCreateCommand(CommandDTO commandDTO){
+       return commandRepository.findCommandBySyntax(commandDTO.syntax()).orElseGet(() ->{ 
         Command newCom= commandMapper.toEntity(commandDTO);
-        if(lArgumentDTOs!=null && !lArgumentDTOs.isEmpty()){
-            updateCommandArguments(newCom, lArgumentDTOs);
-        }
         return this.commandRepository.save(newCom);});
-    }
-
-    private void updateCommandArguments(Command command, List<ArgumentDTO> lArgumentDTOs) {
-        List<Argument> arguments = lArgumentDTOs.stream()
-            .map(dto -> {
-                Argument arg = argumentMapper.toEntity(dto);
-                arg.setCommand(command);
-                return arg;
-            }).toList();
-        
-        if (command.getArgumentsList() == null) {
-            command.setArgumentsList(arguments);
-        } else {
-            command.getArgumentsList().addAll(arguments);
-        }
-
     }
 }
--- backend/src/main/java/aga/easyit/service/FlashCardService.java
+++ backend/src/main/java/aga/easyit/service/FlashCardService.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import aga.easyit.dto.FlashCardDTO;
 import aga.easyit.exception.CardNotFoundException;
-// import aga.easyit.mapper.ArgumentMapper;
 import aga.easyit.model.Command;
 import aga.easyit.model.FlashCard;
 
@@ -14,31 +13,16 @@ import aga.easyit.model.FlashCard;
 public class FlashCardService {
     private final FlashCardRepository flashCardRepository;
     private final CommandService commandService;
-    // private final ArgumentMapper argumentMapper;
     
-    // public FlashCardService(FlashCardRepository flashCardRepository, CommandService commandService, ArgumentMapper argumentMapper) {
     public FlashCardService(FlashCardRepository flashCardRepository, CommandService commandService) {
         this.flashCardRepository = flashCardRepository;
         this.commandService = commandService;
-        // this.argumentMapper=argumentMapper;
     }
 
     @Transactional
     public FlashCard createFlashCard(FlashCardDTO fcDTO) {
-        // Command command;
-        //Step I Create command
-        //Scenario I: With commandDTO 
         Command command = commandService.getOrCreateCommand(fcDTO.commandDTO());
-
-        // if (fcDTO.rawCommandString() != null && !fcDTO.rawCommandString().isBlank()) {
-            
-        // }   
-        // else{
-        //     command=commandService.getOrCreateCommand(fcDTO.commandDTO(), fcDTO.argumentDTOs());
-        // }
-        // //add part with argument
-        FlashCard flashCard = new FlashCard(fcDTO.title(), command);
-        
+        FlashCard flashCard = new FlashCard(fcDTO.title(), command); 
         return flashCardRepository.save(flashCard);
     }
 
